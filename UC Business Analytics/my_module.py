b6dee8fe06f0e5ec65b717d83a28e99469f32af1
cswalthour <cswalthour@gmail.com>
2022-01-14 06:48:11 -0500

    Updates as of 1/14/22

--- UC Business Analytics/my_module.py
+++ UC Business Analytics/my_module.py
@@ -6,13 +6,13 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 def get_features_and_target(csv_file, target_col):
     '''Split a CSV into a DF of numeric features and a target column.'''
     
-    adult_census = pd.read_csv(csv_file)
+    raw_data = pd.read_csv(csv_file)
     
-    raw_features = adult_census.drop(columns=target_col)
+    raw_features = raw_data.drop(columns=target_col)
     numeric_features = raw_features.select_dtypes(np.number)
     feature_cols = numeric_features.columns.values
 
-    features = adult_census[feature_cols]
-    target = adult_census[target_col]
+    features = raw_data[feature_cols]
+    target = raw_data[target_col]
     
     return (features, target)
